fc26a4844887dad0c8f8ed315276ca1bfd798c05
gejx2010 <gejx2010@gmail.com>
2019-08-04 12:28:18 +0800

    finish insert interval

--- insert_interval/main.cpp
+++ insert_interval/main.cpp
@@ -21,12 +21,57 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+      vector<vector<int>> res;
+      if (intervals.empty()) { res.push_back(newInterval); return res; }
+      int nis = newInterval[0], nie = newInterval[1];
+      bool ht = false, track = false;
+      int rk = 0;
+      int st, ed;
+      for (auto v: intervals) {
+        int cs = v[0], ce = v[1];
+        if (!track) {
+          // test start
+          if (!ht) {
+            if (nis < cs) {
+              ht = true;
+              st = nis;
+            } else if (nis <= ce) {
+              ht = true;
+              st = cs;
+            } else {
+              res.push_back(v);
+            }
+          }
+
+          if (ht) {
+            if (nie < cs) {
+              track = true;
+              res.push_back({st, nie});
+              res.push_back(v);
+            } else if (nie <= ce) {
+              track = true;
+              res.push_back({st, ce});
+            } else {
+            }
+          }
+        } else {
+          res.push_back(v);
+        }
+      }
+      if (!track) {
+        if (!ht) { st = nis; }
+        res.push_back({st, nie});
+      }
+      return res;
     }
 };
 
 int main() {
   Solution slt;
-  vector<int> n = {1, 2, 3};
-  PR(slt.maxSubArray(n));
+  vector<vector<int>> n = {{1,2},{3,5},{6,7},{8,10},{12,16}};
+  vector<int> nn = {4,8};
+  for (auto v: slt.insert(n, nn)) {
+    printf ("(%d, %d)\n", v[0], v[1]);
+  }
   return 0;
 }
